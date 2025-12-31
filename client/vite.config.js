e1ccd51ab83bd710788451a8a7604f1b47d234ec
cangle007 <cang.b.le@gmail.com>
2025-12-31 03:11:40 -0800

    add yaml file

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/pokemonAPI-elevenlabs.io/',
   server: {
     proxy: {
       '/api': 'http://localhost:8787',
